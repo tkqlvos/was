11f83b663ff01acdaff9f7924fa41cd6485e48ff
tkqlvos <>
2021-07-25 21:42:43 +0900

    git init

---
+++ src/test/java/kr/co/was/WasApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
